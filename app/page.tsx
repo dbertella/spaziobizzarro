427f54da4726d30986e8711b00aee15e58cebc7c
Daniele Bertella <daniele.bertella@gmail.com>
2024-02-26 23:14:18 +0000

    Add faust

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,32 @@
+import { gql } from "@apollo/client";
+import { getClient } from "@faustwp/experimental-app-router";
 import Image from "next/image";
+import Link from "next/link";
 
-export default function Home() {
+type Post = {
+  id: string
+  title: string
+  uri: string
+  slug: string
+}
+
+export default async function Home() {
+  let client = await getClient();
+
+  const { data } = await client.query({
+    query: gql`
+      query GetPosts {
+        posts {
+          nodes {
+            id
+            title
+            uri
+            slug
+          }
+        }
+      }
+    `,
+  });
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
@@ -108,6 +134,14 @@ export default function Home() {
           </p>
         </a>
       </div>
+      <h2>Posts</h2>
+      <ul>
+        {data.posts.nodes.map((post: Post) => (
+          <li key={post.id}>
+            <Link href={`/${post.slug}`}>{post.title}</Link>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
